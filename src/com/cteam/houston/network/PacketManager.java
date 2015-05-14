809077ac1ee3fecfbce2fc1233c87b88d0b0f2c6
Diego Waxemberg <diego.waxemberg@gmail.com>
2015-05-14 18:31:16 -0400

    added support for reconnecting and moved networking to background thread

--- src/com/cteam/houston/network/PacketManager.java
+++ src/com/cteam/houston/network/PacketManager.java
@@ -2,19 +2,26 @@ package com.cteam.houston.network;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class PacketManager {
 	private static final String HOST = "192.168.7.2";
 	private static final int PORT = 9001;
-
+	private static final int RETRY_RATE = 250; // ms
+	private static final int CONNECT_TIMEOUT = 1000; // ms
+	private static final int MAX_QUEUE_SIZE = 100;
+	
 	private static PacketManager instance;
 	
 	private Socket socket;
+	private LinkedBlockingQueue<Packet> packetQueue;
 	
-	private boolean isSetUp;
-	
-	private PacketManager() {}
+	private PacketManager() {
+		packetQueue = new LinkedBlockingQueue<>(MAX_QUEUE_SIZE);
+		
+	}
 	
 	public static PacketManager instance() {
 		if (instance == null) {
@@ -23,17 +30,31 @@ public class PacketManager {
 		return instance;
 	}
 	
-	public boolean setUp() {
-		if (isSetUp) return true;
-		isSetUp = true;
-		try {
-			socket = new Socket(HOST, PORT);
-		} catch (Exception except) {
-			System.err.println("Houston, we have a problem: Failed to create socket.\n" 
-					+ except.getMessage());
-			isSetUp = false;
-		}
-		return isSetUp;
+	public void setUp() {
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				// This will block until a connection is established or we are told to bail.
+				if (!establishConnection()) return; 
+				while (true) {
+					try {
+						Packet packet = packetQueue.take();
+						while (!sendPacketInternal(packet)) {
+							// There is an issue with the connection, try to reestablish.
+							try {
+								socket.close();
+							} catch (IOException e) {}
+							socket = null;
+							// This will block until a connection is established or we are told to bail.
+							if (!establishConnection()) return;
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}, "Network Thread").start();
 	}
 	
 	public void tearDown() {
@@ -45,16 +66,55 @@ public class PacketManager {
 				except.printStackTrace();
 			}
 		}
-		isSetUp = false;
 	}
 	
 	public void sendPacket(Packet packet) {
+		if (socket != null && socket.isConnected()) {
+			try {
+				packetQueue.add(packet);
+			} catch (IllegalStateException except) {
+				// This occurs when the queue is full. Let's throw out the oldest packet and add this one.
+				packetQueue.remove();
+				packetQueue.add(packet);
+			}
+		}
+	}
+	
+	private boolean establishConnection() {
+		InetSocketAddress address = new InetSocketAddress(HOST, PORT);
+		
+		while (socket == null) {
+			try {
+				socket = new Socket();
+				socket.connect(address, CONNECT_TIMEOUT);
+				socket.setKeepAlive(true);
+			} catch (Exception except) {
+				socket = null;
+				
+				System.err.println("Houston, we have a problem: Failed to create socket. Retrying...\n");
+				except.printStackTrace(System.err);
+				
+				try {
+					Thread.sleep(RETRY_RATE);
+				} catch (InterruptedException e) {
+					// We are bailing out
+					System.err.println("Houston, we are interrupted. Bailing out!");
+					return false;
+				}
+			}
+		}
+		System.out.println("Successfuly connected to Apollo 13!");
+		return true;
+	}
+	
+	private boolean sendPacketInternal(Packet packet) {
 		try {
 			OutputStream stream = socket.getOutputStream();
 			stream.write(packet.getPayload());
 			stream.flush();
+			return true;
 		} catch (IOException e) {
-			e.printStackTrace();
+			return false;
 		}
 	}
 }
